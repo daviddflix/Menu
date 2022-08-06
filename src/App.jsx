08288ec59cbc7bab20946c6aa860c117124c0af1
davidflix <david-972010@hotmail.com>
2022-08-06 19:06:35 -0300

    images

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import img from  './App.css'
+import portada from './assets/portada.jpg'
+import pastas from './assets/pastas.jpg'
+import salsas from './assets/salsas.jpg'
+import extras from './assets/extras2.jpg'
+import doc from './assets/pdf.pdf'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <a href={doc} download={doc} className="btn">Descargar</a>
+       <img className='img' src={portada} alt="portada" />
+       <img className='img' src={pastas} alt="pastas" />
+       <img className='img' src={salsas} alt="salsas" />
+       <img className='img' src={extras} alt="extras" />
     </div>
   )
 }
